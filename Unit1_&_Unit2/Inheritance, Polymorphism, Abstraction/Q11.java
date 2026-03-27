3c0564ebc173529f7ee11a565cfd492ab17ffb32
Tanishq7276 <tanishqgupta070@gmail.com>
2026-03-27 22:19:01 +0530

    Update Q11.java

--- Unit1_&_Unit2/Inheritance, Polymorphism, Abstraction/Q11.java
+++ Unit1_&_Unit2/Inheritance, Polymorphism, Abstraction/Q11.java
@@ -1,47 +1,71 @@
+import java.util.Scanner;
+
+// Base class
 class Shape {
     void display() {
         System.out.println("This is a Shape");
     }
 }
 
-// Circle class inherits Shape
+// Derived class Circle
 class Circle extends Shape {
     double radius;
 
+    // Constructor
     Circle(double r) {
         radius = r;
     }
 
-    void area() {
-        double a = 3.14 * radius * radius;
-        System.out.println("Area of Circle: " + a);
+    // Method to calculate area of circle
+    void calculateArea() {
+        double area = 3.14 * radius * radius;
+        System.out.println("Area of Circle: " + area);
     }
 }
 
-// Rectangle class inherits Shape
+// Derived class Rectangle
 class Rectangle extends Shape {
     double length, breadth;
 
+    // Constructor
     Rectangle(double l, double b) {
         length = l;
         breadth = b;
     }
 
-    void area() {
-        double a = length * breadth;
-        System.out.println("Area of Rectangle: " + a);
+    // Method to calculate area of rectangle
+    void calculateArea() {
+        double area = length * breadth;
+        System.out.println("Area of Rectangle: " + area);
     }
 }
 
+// Main class
 public class Main {
     public static void main(String[] args) {
-        Circle c = new Circle(5);
-        Rectangle r = new Rectangle(4, 6);
+        Scanner sc = new Scanner(System.in);
 
+        // Input for circle
+        System.out.print("Enter radius of circle: ");
+        double r = sc.nextDouble();
+
+        // Input for rectangle
+        System.out.print("Enter length of rectangle: ");
+        double l = sc.nextDouble();
+        System.out.print("Enter breadth of rectangle: ");
+        double b = sc.nextDouble();
+
+        // Create objects
+        Circle c = new Circle(r);
+        Rectangle rect = new Rectangle(l, b);
+
+        // Display and calculate area
+        System.out.println("\n--- Circle ---");
         c.display();
-        c.area();
+        c.calculateArea();
 
-        r.display();
-        r.area();
+        System.out.println("\n--- Rectangle ---");
+        rect.display();
+        rect.calculateArea();
     }
 }
